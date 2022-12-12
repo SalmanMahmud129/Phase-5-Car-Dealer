0b142fed00df85d09498581ae363da7c9fe57107
Salman Mahmud <salman_mahmud@hotmail.com>
2022-12-12 15:00:09 -0500

    Configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Routes>
+
+        <Route path="/testing" element={<h1>Test Route</h1>}/>
+          
+
+        <Route path="/" element={<h1>Page Count: {count}</h1>}/>
+        
+      </Routes>
+      
     </div>
   );
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
 
   get '/hello', to: 'application#hello_world'
+
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
