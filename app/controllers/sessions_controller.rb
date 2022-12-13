677a830844357a9a7494a643d374f73ead46fefc
Salman Mahmud <salman_mahmud@hotmail.com>
2022-12-12 19:41:16 -0500

    commented out all authorization elements for current deployment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorized, only: :create
+    # skip_before_action :authorized, only: :create
 
     def create
         user = User.find_by(username: params[:username])
