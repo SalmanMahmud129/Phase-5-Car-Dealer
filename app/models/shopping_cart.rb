a4b10b59a595bfe4bf78751f513f4b2caec4186c
Salman Mahmud <salman_mahmud@hotmail.com>
2023-01-03 22:47:27 -0500

    attempt to fix foreign key issue on deployment 3rd time

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,5 +1,5 @@
 class ShoppingCart < ApplicationRecord
   belongs_to :user
-  has_many :cart_vehicles
+  has_many :cart_vehicles, dependent: :destroy
   has_many :vehicles, through: :cart_vehicles
 end
